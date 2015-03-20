b5d411d90447b161e7dd16886db4c11cb2123bd0
Sönke Buhr <sonke.buhr@kabelmail.de>
2015-03-21 00:43:52 +0100

    Added data_class support for attributes
    Added data_class support for attributes

--- lib/sax-machine/config/sax_element_value.rb
+++ lib/sax-machine/config/sax_element_value.rb
@@ -1,13 +1,14 @@
 module SAXMachine
   class SAXConfig
     class ElementValueConfig
-      attr_reader :name, :setter
+      attr_reader :name, :setter, :data_class
 
       def initialize(name, options)
         @name     = name.to_s
         @as       = options[:as]
         @setter   = "#{@as}="
         @required = options[:required]
+        @data_class = options[:class]
       end
 
       def column
