ccc11c267e1f6931f2934bc9cb0b3c6244ec8716
Antony Bowen <Antsiscool@users.noreply.github.com>
2021-01-05 11:47:46 +1100

    Remove deprecated use of BigDecimal#new

--- spec/sax-machine/sax_document_spec.rb
+++ spec/sax-machine/sax_document_spec.rb
@@ -265,17 +265,17 @@ describe "SAXMachine" do
 
           it "is handled in an element with '.' delimiter" do
             document = TestBigDecimal.parse("<number>3.0000000000000021</number>")
-            expect(document.number).to eq(BigDecimal.new('3.0000000000000021'))
+            expect(document.number).to eq(BigDecimal('3.0000000000000021'))
           end
 
           it "is handled in an element with ',' delimiter" do
             document = TestBigDecimal.parse("<number>3,0000000000000021</number>")
-            expect(document.number).to eq(BigDecimal.new('3.0000000000000021'))
+            expect(document.number).to eq(BigDecimal('3.0000000000000021'))
           end
 
           it "is handled in an attribute" do
             document = TestBigDecimalWithAttribute.parse("<number sub_number='3.0000000000000021'>3.0000000000000021</number>")
-            expect(document.number.sub_number).to eq(BigDecimal.new('3.0000000000000021'))
+            expect(document.number.sub_number).to eq(BigDecimal('3.0000000000000021'))
           end
         end
 
